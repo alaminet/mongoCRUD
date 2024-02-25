ecdd0ab589abdaf7f8cb8c7390b7eb7f242fcdd1
Al Amin ET <alaminet.bd@gmail.com>
2024-02-25 23:28:12 +0600

    update index db config

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require("express");
 const app = express();
 const route = require("./routes");
 const cors = require("cors");
-const mongoConfig = require('./config/mongoConfig')
+const mongoConfig = require("./config/mongoConfig");
 
-mongoConfig()
+mongoConfig();
 app.use(cors());
 app.use(express.json());
 
 app.use(route);
 
-port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`server is running port ${port}`);
 });
